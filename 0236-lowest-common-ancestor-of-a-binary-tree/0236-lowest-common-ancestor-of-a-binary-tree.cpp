de211d079516d5713f5a0013a147de6b7c439eda
shaikhsamir07 <samirshaikh.dev@gmail.com>
2026-05-17 22:42:38 +0530

    Time: 13 ms (50.43%), Space: 17.5 MB (14.47%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,24 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
 class Solution {
 public:
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    TreeNode* lowestCommonAncestor(TreeNode* root,
+                                   TreeNode* p,
+                                   TreeNode* q) {
+
         // Base case
         if (root == NULL || root == p || root == q) {
             return root;
         }
 
-        // Search in left subtree
-        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* left =
+            lowestCommonAncestor(root->left, p, q);
 
-        // Search in right subtree
-        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        TreeNode* right =
+            lowestCommonAncestor(root->right, p, q);
 
-        // If both sides return non-null,
-        // current node is the LCA
-        if (left && right) {
+        // Both nodes found in different subtrees
+        if (left != NULL && right != NULL) {
             return root;
         }
 
-        // Otherwise return the non-null child
+        // Return non-null subtree
         return (left != NULL) ? left : right;
     }
 };
