60f14cda785aacd93fc76943870a87a3ca4ba877
shaikhsamir07 <samirshaikh.dev@gmail.com>
2026-05-17 23:28:49 +0530

    Time: 1 ms (33.69%), Space: 20.7 MB (92.82%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -0,0 +1,75 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+
+        bool firstRowZero = false;
+        bool firstColZero = false;
+
+        // Check first row
+        for (int j = 0; j < cols; j++) {
+
+            if (matrix[0][j] == 0) {
+
+                firstRowZero = true;
+                break;
+            }
+        }
+
+        // Check first column
+        for (int i = 0; i < rows; i++) {
+
+            if (matrix[i][0] == 0) {
+
+                firstColZero = true;
+                break;
+            }
+        }
+
+        // Mark rows and columns
+        for (int i = 1; i < rows; i++) {
+
+            for (int j = 1; j < cols; j++) {
+
+                if (matrix[i][j] == 0) {
+
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // Set zeroes using markers
+        for (int i = 1; i < rows; i++) {
+
+            for (int j = 1; j < cols; j++) {
+
+                if (matrix[i][0] == 0 ||
+                    matrix[0][j] == 0) {
+
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Set first row to zero
+        if (firstRowZero) {
+
+            for (int j = 0; j < cols; j++) {
+
+                matrix[0][j] = 0;
+            }
+        }
+
+        // Set first column to zero
+        if (firstColZero) {
+
+            for (int i = 0; i < rows; i++) {
+
+                matrix[i][0] = 0;
+            }
+        }
+    }
+};
