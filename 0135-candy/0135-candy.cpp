d25fecf476409e088b7f2c34c5fee368eba3c96c
shaikhsamir07 <samirshaikh.dev@gmail.com>
2026-05-21 21:11:30 +0530

    Time: 0 ms (100%), Space: 23.1 MB (28.42%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -7,6 +7,7 @@ public:
         vector<int> candies(n, 1);
 
         // Left to right
+
         for (int i = 1; i < n; i++) {
 
             if (ratings[i] > ratings[i - 1]) {
@@ -16,12 +17,13 @@ public:
         }
 
         // Right to left
+
         for (int i = n - 2; i >= 0; i--) {
 
             if (ratings[i] > ratings[i + 1]) {
 
                 candies[i] = max(candies[i],
-                                  candies[i + 1] + 1);
+                                 candies[i + 1] + 1);
             }
         }
 
