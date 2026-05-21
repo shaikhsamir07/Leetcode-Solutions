fc0899abdaa15a145344c5658d3df69a9ecf4d92
shaikhsamir07 <samirshaikh.dev@gmail.com>
2026-05-21 18:57:16 +0530

    Time: 11 ms (13.51%), Space: 25.8 MB (9.22%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+
+        unordered_set<string> seen;
+
+        for (int i = 0; i < 9; i++) {
+
+            for (int j = 0; j < 9; j++) {
+
+                char num = board[i][j];
+
+                if (num == '.') {
+                    continue;
+                }
+
+                string row =
+                    string(1, num) +
+                    " in row " + to_string(i);
+
+                string col =
+                    string(1, num) +
+                    " in col " + to_string(j);
+
+                string box =
+                    string(1, num) +
+                    " in box " +
+                    to_string(i / 3) +
+                    "-" +
+                    to_string(j / 3);
+
+                // Duplicate found
+                if (seen.count(row) ||
+                    seen.count(col) ||
+                    seen.count(box)) {
+
+                    return false;
+                }
+
+                seen.insert(row);
+                seen.insert(col);
+                seen.insert(box);
+            }
+        }
+
+        return true;
+    }
+};
