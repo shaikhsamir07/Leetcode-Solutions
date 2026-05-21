d1a07475c6f908a430b2f2e817e35d5f544c02a2
shaikhsamir07 <samirshaikh.dev@gmail.com>
2026-05-21 18:49:13 +0530

    Time: 3 ms (2.34%), Space: 8.7 MB (91.59%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+
+        int i = s.length() - 1;
+        int length = 0;
+
+        // Skip trailing spaces
+        while (i >= 0 && s[i] == ' ') {
+            i--;
+        }
+
+        // Count last word length
+        while (i >= 0 && s[i] != ' ') {
+
+            length++;
+            i--;
+        }
+
+        return length;
+    }
+};
