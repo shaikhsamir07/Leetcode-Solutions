c2d549a317fce24f78bbabec97c1196313085f32
shaikhsamir07 <samirshaikh.dev@gmail.com>
2026-05-21 19:10:42 +0530

    Time: 13 ms (18.89%), Space: 11.7 MB (79.78%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -14,11 +14,9 @@ public:
         ListNode* slow = head;
         ListNode* fast = head;
 
-        while (fast != NULL &&
-               fast->next != NULL) {
+        while (fast != NULL && fast->next != NULL) {
 
             slow = slow->next;
-
             fast = fast->next->next;
 
             // Cycle detected
