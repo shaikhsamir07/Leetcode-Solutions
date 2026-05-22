4e74cd135bbfeaa9f9bf21b5c74f7713c972eacd
shaikhsamir07 <samirshaikh.dev@gmail.com>
2026-05-22 20:23:44 +0530

    Time: 0 ms (100%), Space: 11.1 MB (79.88%) - LeetHub

---
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+
+    int rangeBitwiseAnd(int left, int right) {
+
+        int shift = 0;
+
+        // Find common prefix
+        while (left < right) {
+
+            left >>= 1;
+            right >>= 1;
+
+            shift++;
+        }
+
+        return left << shift;
+    }
+};
