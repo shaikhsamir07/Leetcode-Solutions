72de344c8beb9e6bc55c0d14762c1619d4fb0f33
shaikhsamir07 <samirshaikh.dev@gmail.com>
2026-05-23 17:24:32 +0530

    Time: 74 ms (62.93%), Space: 182.1 MB (33.05%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,32 +1,27 @@
 class LRUCache {
 public:
+    int cap;
 
-    int capacity;
-
+    // Doubly linked list stores {key, value}
     list<pair<int, int>> cache;
 
+    // key -> iterator of list
     unordered_map<int, list<pair<int, int>>::iterator> mp;
 
     LRUCache(int capacity) {
-
-        this->capacity = capacity;
+        cap = capacity;
     }
 
     int get(int key) {
-
-        // Key not found
         if (mp.find(key) == mp.end()) {
-
             return -1;
         }
 
         // Move accessed node to front
-        auto node = mp[key];
+        auto it = mp[key];
+        int value = it->second;
 
-        int value = node->second;
-
-        cache.erase(node);
-
+        cache.erase(it);
         cache.push_front({key, value});
 
         mp[key] = cache.begin();
@@ -38,22 +33,18 @@ public:
 
         // If key already exists
         if (mp.find(key) != mp.end()) {
-
             cache.erase(mp[key]);
         }
 
         // Insert at front
         cache.push_front({key, value});
-
         mp[key] = cache.begin();
 
-        // Remove least recently used
-        if (cache.size() > capacity) {
-
+        // Remove LRU if capacity exceeded
+        if (cache.size() > cap) {
             auto last = cache.back();
 
             mp.erase(last.first);
-
             cache.pop_back();
         }
     }
