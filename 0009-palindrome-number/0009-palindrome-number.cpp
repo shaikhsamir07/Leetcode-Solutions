9aad0588ff6f457cac18ab99cf35cc586eaaca41
shaikhsamir07 <samirshaikh.dev@gmail.com>
2026-05-23 16:53:21 +0530

    Time: 2 ms (50.93%), Space: 8.4 MB (91.64%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-
     bool isPalindrome(int x) {
-
-        // Negative numbers are not palindrome
-        // Numbers ending with 0 (except 0) are not palindrome
-        if (x < 0 || (x % 10 == 0 && x != 0))
+        // Negative numbers and numbers ending with 0
+        // (except 0 itself) cannot be palindrome
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
+        }
 
         int reversed = 0;
 
-        // Reverse only half of the number
         while (x > reversed) {
-
             reversed = reversed * 10 + x % 10;
             x /= 10;
         }
 
-        // Even digits: x == reversed
-        // Odd digits: x == reversed / 10
+        // For even digits: x == reversed
+        // For odd digits: x == reversed / 10
         return (x == reversed || x == reversed / 10);
     }
 };
