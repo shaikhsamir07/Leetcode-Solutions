1d2244ab7993a6015754193da9fcee784ba18d81
shaikhsamir07 <samirshaikh.dev@gmail.com>
2026-05-23 17:28:47 +0530

    Time: 26 ms (63.11%), Space: 16.2 MB (77.41%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,49 +1,41 @@
 class Solution {
 public:
-
-    void dfs(vector<vector<char>>& grid,
-             int row,
-             int col) {
-
+    void dfs(vector<vector<char>>& grid, int i, int j) {
         int m = grid.size();
         int n = grid[0].size();
 
-        // Out of bounds or water
-        if (row < 0 || col < 0 ||
-            row >= m || col >= n ||
-            grid[row][col] == '0') {
+        // Boundary check
+        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == '0') {
             return;
         }
 
-        // Mark visited
-        grid[row][col] = '0';
+        // Mark as visited
+        grid[i][j] = '0';
 
-        dfs(grid, row + 1, col);
-        dfs(grid, row - 1, col);
-        dfs(grid, row, col + 1);
-        dfs(grid, row, col - 1);
+        // Explore 4 directions
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
     }
 
     int numIslands(vector<vector<char>>& grid) {
-
         int m = grid.size();
         int n = grid[0].size();
 
-        int islands = 0;
+        int count = 0;
 
         for (int i = 0; i < m; i++) {
-
             for (int j = 0; j < n; j++) {
 
                 if (grid[i][j] == '1') {
-
-                    islands++;
+                    count++;
 
                     dfs(grid, i, j);
                 }
             }
         }
 
-        return islands;
+        return count;
     }
 };
